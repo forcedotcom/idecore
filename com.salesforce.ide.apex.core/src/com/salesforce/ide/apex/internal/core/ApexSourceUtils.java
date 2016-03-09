27eb24b00bfe844677f7945ec626bcbb7e2d563f
Nick Chen <nchen@salesforce.com>
2016-03-09 13:55:00 -0800

    Change the way we find test classes and test methods
    @W-2960472@

--- com.salesforce.ide.apex.core/src/com/salesforce/ide/apex/internal/core/ApexSourceUtils.java
+++ com.salesforce.ide.apex.core/src/com/salesforce/ide/apex/internal/core/ApexSourceUtils.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 Salesforce.com, inc..
+ * Copyright (c) 2016 Salesforce.com, inc..
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -9,6 +9,9 @@
  *     Salesforce.com, inc. - initial API and implementation
  ******************************************************************************/
 package com.salesforce.ide.apex.internal.core;
+
+import static apex.jorje.semantic.symbol.type.AnnotationTypeInfos.IS_TEST;
+import static apex.jorje.semantic.symbol.type.ModifierTypeInfos.TEST_METHOD;
 
 import java.util.Arrays;
 import java.util.List;
@@ -31,13 +34,11 @@ import com.salesforce.ide.core.model.Component;
 import com.salesforce.ide.core.services.ProjectService;
 
 import apex.jorje.data.Loc.RealLoc;
-import apex.jorje.data.ast.BlockMember;
-import apex.jorje.data.ast.BlockMember.MethodMember;
-import apex.jorje.data.ast.CompilationUnit;
-import apex.jorje.data.ast.CompilationUnit.ClassDeclUnit;
-import apex.jorje.data.ast.Modifier;
-import apex.jorje.data.ast.Modifier.Annotation;
-import apex.jorje.data.ast.Modifier.TestMethodModifier;
+import apex.jorje.semantic.ast.compilation.UserClass;
+import apex.jorje.semantic.ast.member.Method;
+import apex.jorje.semantic.ast.modifier.ModifierGroup;
+import apex.jorje.semantic.ast.visitor.AdditionalPassScope;
+import apex.jorje.semantic.ast.visitor.AstVisitor;
 
 /**
  * Utilities for working on Apex source files.
@@ -46,25 +47,6 @@ import apex.jorje.data.ast.Modifier.TestMethodModifier;
  * 
  */
 public class ApexSourceUtils {
-    private final class TestModifierDeterminer extends Modifier.SwitchBlockWithDefault {
-        boolean hasSeenTestModifier;
-
-        @Override
-        public void _case(TestMethodModifier x) {
-            hasSeenTestModifier = true;
-        }
-
-        @Override
-        public void _case(Annotation x) {
-            if (x.name.value.equalsIgnoreCase("istest")) {
-                hasSeenTestModifier = true;
-            }
-        }
-
-        @Override
-        protected void _default(Modifier x) {}
-    }
-
     private static final Logger logger = Logger.getLogger(ApexSourceUtils.class);
 
     public static final ApexSourceUtils INSTANCE = new ApexSourceUtils();
@@ -107,32 +89,26 @@ public class ApexSourceUtils {
         final List<String> methodNames = Lists.newArrayList();
 
         try {
-            IFile file = (IFile) resource;
-            CompilationUnit compilationUnit = ApexModelManager.INSTANCE.getCompilationUnit(file);
-            compilationUnit._switch(new CompilationUnit.SwitchBlockWithDefault() {
+            CompilerService.INSTANCE.visitAstFromFile((IFile) resource, new AstVisitor<AdditionalPassScope>() {
 
                 @Override
-                public void _case(ClassDeclUnit classDeclUnit) {
-                    if (classDeclUnit.body != null && classDeclUnit.body.members != null) {
-                        for (BlockMember member : classDeclUnit.body.members.values) {
-                            member._switch(new BlockMember.SwitchBlockWithDefault() {
-
-                                @Override
-                                public void _case(MethodMember x) {
-                                    if (hasTestModifier(x.methodDecl.modifiers)) {
-                                        methodNames.add(x.methodDecl.name.value);
-                                    }
-                                }
-
-                                @Override
-                                protected void _default(BlockMember x) {}
-                            });
-                        }
-                    }
+                public boolean visit(UserClass node, AdditionalPassScope scope) {
+                    return true;
                 }
 
                 @Override
-                protected void _default(CompilationUnit x) {}
+                public void visitEnd(UserClass node, AdditionalPassScope scope) {
+                    super.visitEnd(node, scope);
+                }
+
+                @Override
+                public boolean visit(Method method, AdditionalPassScope scope) {
+                    if (isTestMethod(method.getModifiers())) {
+                        methodNames.add(method.getMethodInfo().getName());
+                    }
+                    return super.visit(method, scope);
+                }
+                
             });
         } catch (Exception e) {
             logger.error("Encountered an issue trying to find test method names for " + resource.getName(), e);
@@ -142,7 +118,7 @@ public class ApexSourceUtils {
     }
     
     /**
-     * Find location of test methdods in a given resource.
+     * Find location of test methods in a given resource.
      * Test methods are annotated with @IsTest or 'testmethod'.
      * 
      * @param resource
@@ -152,34 +128,28 @@ public class ApexSourceUtils {
     	final Map<String, ApexCodeLocation> testMethods = Maps.newHashMap();
     	
     	try {
-            final IFile file = (IFile) resource;
-            CompilationUnit compilationUnit = ApexModelManager.INSTANCE.getCompilationUnit(file);
-            compilationUnit._switch(new CompilationUnit.SwitchBlockWithDefault() {
+            CompilerService.INSTANCE.visitAstFromFile((IFile) resource, new AstVisitor<AdditionalPassScope>() {
 
                 @Override
-                public void _case(ClassDeclUnit classDeclUnit) {
-                    if (classDeclUnit.body != null && classDeclUnit.body.members != null) {
-                        for (final BlockMember member : classDeclUnit.body.members.values) {
-                            member._switch(new BlockMember.SwitchBlockWithDefault() {
-
-                                @Override
-                                public void _case(MethodMember x) {
-                                    if (hasTestModifier(x.methodDecl.modifiers)) {
-                                    	RealLoc realLoc = (RealLoc) x.methodDecl.name.loc;
-                                    	ApexCodeLocation loc = new ApexCodeLocation(file, realLoc.line, realLoc.column);
-                                    	testMethods.put(x.methodDecl.name.value, loc);
-                                    }
-                                }
-
-                                @Override
-                                protected void _default(BlockMember x) {}
-                            });
-                        }
-                    }
+                public boolean visit(UserClass node, AdditionalPassScope scope) {
+                    return true;
                 }
 
                 @Override
-                protected void _default(CompilationUnit x) {}
+                public void visitEnd(UserClass node, AdditionalPassScope scope) {
+                    super.visitEnd(node, scope);
+                }
+
+                @Override
+                public boolean visit(Method method, AdditionalPassScope scope) {
+                    if (isTestMethod(method.getModifiers())) {
+                        RealLoc realLoc = (RealLoc) method.getLoc();
+                        ApexCodeLocation loc = new ApexCodeLocation((IFile) resource, realLoc.line, realLoc.column);
+                        testMethods.put(method.getMethodInfo().getCanonicalName(), loc);
+                    }
+                    return super.visit(method, scope);
+                }
+                
             });
         } catch (Exception e) {
             logger.error("Encountered an issue trying to find test methods for " + resource.getName(), e);
@@ -196,47 +166,24 @@ public class ApexSourceUtils {
      * @return Location of class decl
      */
     public ApexCodeLocation findClassLocInFile(IResource resource) {
-    	final class Pair {
-    		private final int line;
-    		private final int col;
-    		
-    		public Pair(int line, int col) {
-    			this.line = line;
-    			this.col = col;
-    		}
-    		
-    		public int getLine() {
-    			return this.line;
-    		}
-    		
-    		public int getCol() {
-    			return this.col;
-    		}
-    	}
-    	
-    	final IFile file = (IFile) resource;
-    	
-    	try {
-    		CompilationUnit compilationUnit = ApexModelManager.INSTANCE.getCompilationUnit(file);
-        	Pair match = compilationUnit.match(new CompilationUnit.MatchBlockWithDefault<Pair>() {
-        		@Override
-                public Pair _case(ClassDeclUnit classDeclUnit) {
-        			RealLoc realLoc = (RealLoc) classDeclUnit.body.name.loc;
-					return new Pair(realLoc.line, realLoc.column);
-        		}
-
-        		@Override
-                protected Pair _default(CompilationUnit x) {
-        			return new Pair(1, 1);
-        		}
-        	});
-        	
-        	return new ApexCodeLocation(file, match.getLine(), match.getCol());
-    	} catch (Exception e) {
+        ApexCodeLocation[] returnValue = new ApexCodeLocation[1];
+        try {
+            CompilerService.INSTANCE.visitAstFromFile((IFile) resource, new AstVisitor<AdditionalPassScope>() {
+                
+                @Override
+                public boolean visit(UserClass userClass, AdditionalPassScope scope) {
+                    RealLoc realLoc = (RealLoc) userClass.getLoc();
+                    returnValue[0] = new ApexCodeLocation((IFile) resource, realLoc.line, realLoc.column);
+                    return super.visit(userClass, scope);
+                }
+                
+            });
+            return returnValue[0];
+        } catch (Exception e) {
             logger.error("Encountered an issue trying to find location of class declaration " + resource.getName(), e);
         }
-    	
-    	return null;
+        
+        return null;
     }
     
     /**
@@ -324,25 +271,7 @@ public class ApexSourceUtils {
         }
     }
 
-    /**
-     * Check if a list of modifiers has a test modifier.
-     * 
-     * @param modifiers
-     * @return True if there is a test modifier. False otherwise.
-     */
-    private boolean hasTestModifier(List<Modifier> modifiers) {
-        if (modifiers == null || modifiers.isEmpty()) {
-            return false;
-        }
-
-        for (Modifier modifier : modifiers) {
-            TestModifierDeterminer switchBlock = new TestModifierDeterminer();
-            modifier._switch(switchBlock);
-
-            if (switchBlock.hasSeenTestModifier)
-                return true;
-        }
-
-        return false;
+    private boolean isTestMethod(ModifierGroup modifiers) {
+        return modifiers != null && (modifiers.has(TEST_METHOD) || modifiers.has(IS_TEST));
     }
 }
