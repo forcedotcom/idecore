3aabfe78c9f438fd3805099f6ebaa3eb314846ae
Ross <dbaker@salesforce.com>
2016-08-09 15:29:23 -0700

    Added copyright header

--- com.salesforce.ide.ui/src/com/salesforce/ide/ui/handlers/ForceIdeUrlActionHandler.java
+++ com.salesforce.ide.ui/src/com/salesforce/ide/ui/handlers/ForceIdeUrlActionHandler.java
@@ -1,4 +1,13 @@
-package com.salesforce.ide.ui.handlers;
+/*******************************************************************************
+ * Copyright (c) 2016 Salesforce.com, inc..
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Salesforce.com, inc. - initial API and implementation
+ ******************************************************************************/package com.salesforce.ide.ui.handlers;
 
 import java.lang.reflect.InvocationTargetException;
 
