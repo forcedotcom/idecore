161770b23ad3d5e30125659f601dfc44e74a5b6a
Nick Chen <nchen@salesforce.com>
2016-08-16 15:27:19 -0700

    Fix typo and spacing in default-templates.xml
    @W-3279152@

--- com.salesforce.ide.ui.editors/src/com/salesforce/ide/ui/wizards/components/lightning/AuraDefinitionBundleWizardComposite.java
+++ com.salesforce.ide.ui.editors/src/com/salesforce/ide/ui/wizards/components/lightning/AuraDefinitionBundleWizardComposite.java
@@ -23,7 +23,6 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 
-import com.salesforce.ide.core.internal.utils.Utils;
 import com.salesforce.ide.ui.wizards.components.apex.CodeWizardComposite;
 
 /**
